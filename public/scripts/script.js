e839474119ed4fdceb49dd491a9ed998cc9867fd
safe user <todderick@gmail.com>
2014-09-10 15:18:22 -0400

    Make audio and video work, loosely, in chrome

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,7 @@
 var stream = undefined;
 var isRecording = false;
 var recordRTC = undefined;
-var options = undefined;
+var recordATC = undefined;
 
 navigator.getUserMedia  = navigator.getUserMedia ||
                           navigator.webkitGetUserMedia ||
@@ -15,9 +15,9 @@ function bindConnection(_fn,_me) {
 }
 
 function WebRTC(_options) {
-  options = _options;
+  this.options = _options;
   this.onConnection = bindConnection(this.onConnection, this);
-  navigator.getUserMedia(options, this.onConnection, this.onError);
+  navigator.getUserMedia(this.options, this.onConnection, this.onError);
 }
 
 WebRTC.prototype.onConnection = function(_stream) {
@@ -32,19 +32,28 @@ WebRTC.prototype.onError = function(_error) {
   console.log("Error: ",_error);
 }
 
-WebRTC.prototype.onElClick = function(_ev) {
-  console.log(options);
+WebRTC.prototype.onElClick = function(_ev,index) {
+  var el = _ev.target;
+  var elA = _ev.target.getElementsByTagName('audio')[0];
   if (!isRecording && stream != undefined) {
-    recordRTC = RecordRTC(stream, options);
+    recordRTC = RecordRTC(stream, {type: 'video'});
+    recordATC = RecordRTC(stream);
     recordRTC.startRecording();
+    recordATC.startRecording();
   } else if (isRecording) {
-    recordRTC.stopRecording(function(_url){
-      var el = _ev.target;
+    recordRTC.stopRecording(function(_url){      
       el.src = _url;
-      el.muted = false;
-      el.loop = true;      
-      el.setAttribute('loop',true);
-      el.play();
+    });
+    recordATC.stopRecording(function(_url) {
+      elA.src = _url;
+      elA.muted = false;
+    });
+    el.addEventListener('loadeddata', function() {
+      elA.play();
+      setInterval(function() {
+        elA.currentTime = 0;
+        el.currentTime = 0;
+      },el.duration*1000);
     });
   }
   isRecording = !isRecording;
@@ -52,6 +61,5 @@ WebRTC.prototype.onElClick = function(_ev) {
 
 new WebRTC({
   video: true,
-  audio: true,
-  type: 'video'
+  audio: true
 });
