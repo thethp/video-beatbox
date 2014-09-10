518a54e09676393a794af5bd9193458ae410b769
safe user <todderick@gmail.com>
2014-09-10 11:15:46 -0400

    get audio working though timing is off

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,29 +1,48 @@
-var webRTC;
+var stream = undefined;
+var isRecording = false;
+var recordRTC = undefined;
 navigator.getUserMedia  = navigator.getUserMedia ||
                           navigator.webkitGetUserMedia ||
                           navigator.mozGetUserMedia ||
                           navigator.msGetUserMedia;
 
-function bindConnection(fn,me) {
+function bindConnection(_fn,_me) {
   return function() { 
-    return fn.apply(me, arguments);
+    return _fn.apply(_me, arguments);
   }
 }
 
-function WebRTC(options) {
-  this.options = options;
+function WebRTC(_options) {
+  this.options = _options;
   this.onConnection = bindConnection(this.onConnection, this);
   navigator.getUserMedia(this.options, this.onConnection, this.onError);
 }
 
-WebRTC.prototype.onConnection = function(stream) {
+WebRTC.prototype.onConnection = function(_stream) {
+  stream = _stream;
   for(var els = document.getElementsByTagName('video'),i=0;i<els.length;i++) {
-    els[i].src = URL.createObjectURL(stream);
+    els[i].src = URL.createObjectURL(_stream);
+    els[i].addEventListener('click', this.onElClick);
   }
 }
 
-WebRTC.prototype.onError = function(error) {
-  console.log("Error: ",error);
+WebRTC.prototype.onError = function(_error) {
+  console.log("Error: ",_error);
+}
+
+WebRTC.prototype.onElClick = function(_ev) {
+  if (!isRecording && stream != undefined) {
+    recordRTC = RecordRTC(stream);
+    recordRTC.startRecording();
+  } else if (isRecording) {
+    recordRTC.stopRecording(function(_url){
+      _ev.target.src = _url;
+      _ev.target.removeAttribute('muted');
+      _ev.target.muted = false;
+      _ev.target.play();
+    });
+  }
+  isRecording = !isRecording;
 }
 
 new WebRTC({
